4060be62fc2925b2f4a14526147adda9ecc4359a
DmitryRyabchun <dmytro.ryabchun@gmail.com>
2017-11-08 19:09:05 +0200

    snake

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-$(document).ready(function () {
+﻿$(document).ready(function () {
 
     //Создание поля для игры
     function createField(width, height) {
         for (var i = 0; i < height; i++) {
-            $('.field').append('<div id = "field-line_' + i + '" class="field-line"></div>');
+            $('.field').append(
+                '<div id = "field-line_' + i + '" class="field-line"></div>'
+            );
             for (var j = 0; j < width; j++) {
-                $('#field-line_' + i).append('<div id = "block_' + i + '_' + j + '"></div>');
+                $('#field-line_' + i).append(
+                    '<div id = "block_' + i + '_' + j + '"></div>'
+                );
             }
         }
     }
@@ -31,32 +35,39 @@ $(document).ready(function () {
         }
     }
 
-//    function stat() {
-//        console.log(last_game);
-//                console.log(counter);
-//                if(counter> last_game) {
-//                    $('.best_game').html('<span>' + counter + '</span>');
-//                }
-//                console.log(last_game);
-//                console.log(counter);
-//        if(counter !== 0) {
-//             $('.last_game').html('<span>' + last_game + '</span>');
-//            last_game = counter;
-//        }
-//    }
-    
+    //вывод данных о последней игре и лучшей игре
+    function statusBar() {
+        console.log(last_game);
+        console.log(counter);
+
+        if (counter > best_game) {
+            best_game = counter;
+            $('.best_game').html('<span>' + best_game + '</span>');
+        }
+        console.log(last_game);
+        console.log(counter);
+        last_game = counter;
+        if (counter !== 0) { //исправляет баг с двойной перезагрузкой
+            $('.last_game').html('<span>' + last_game + '</span>');
+        }
+    }
+
     //начинает новую игру, если змейка пересекает сама себя
     function Intersection() {
         for (var ind = length - 5; ind >= 0; ind--) {
             if (snake_x[length - 1] === snake_x[ind] && snake_y[length - 1] === snake_y[ind]) {
                 $('.field').empty();
                 createField(width, height);
-//                stat();
+                statusBar();
                 length = 3,
-                    snake_x = [0, 1, 2],
-                    snake_y = [0, 0, 0],
-                    lastTouch = 40,
-                    counter = 0;
+                snake_x = [
+                    0, 1, 2
+                ],
+                snake_y = [
+                    0, 0, 0
+                ],
+                lastTouch = 40,
+                counter = 0;
                 $('#counter').html('<span>' + counter + '</span>');
                 createSnake();
                 coordinatesApple = randomApple();
@@ -68,16 +79,28 @@ $(document).ready(function () {
     function Overstepping() {
         if (snake_x[length - 1] === width) {
             snake_x[length - 1] = 0;
-            $('#block_' + snake_x[length - 1] + '_' + snake_y[length - 1]).css('background', color_snace);
+            $('#block_' + snake_x[length - 1] + '_' + snake_y[length - 1]).css(
+                'background',
+                color_snace
+            );
         } else if (snake_y[length - 1] === height) {
             snake_y[length - 1] = 0;
-            $('#block_' + snake_x[length - 1] + '_' + snake_y[length - 1]).css('background', color_snace);
+            $('#block_' + snake_x[length - 1] + '_' + snake_y[length - 1]).css(
+                'background',
+                color_snace
+            );
         } else if (snake_x[length - 1] === -1) {
-            snake_x[length - 1] = width-1;
-            $('#block_' + snake_x[length - 1] + '_' + snake_y[length - 1]).css('background', color_snace);
+            snake_x[length - 1] = width - 1;
+            $('#block_' + snake_x[length - 1] + '_' + snake_y[length - 1]).css(
+                'background',
+                color_snace
+            );
         } else if (snake_y[length - 1] === -1) {
-            snake_y[length - 1] = height-1;
-            $('#block_' + snake_x[length - 1] + '_' + snake_y[length - 1]).css('background', color_snace);
+            snake_y[length - 1] = height - 1;
+            $('#block_' + snake_x[length - 1] + '_' + snake_y[length - 1]).css(
+                'background',
+                color_snace
+            );
         }
     }
 
@@ -91,7 +114,10 @@ $(document).ready(function () {
         route(lastTouch);
         Intersection();
         $('#block_' + snake_x[0] + '_' + snake_y[0]).css('background', color_snace);
-        $('#block_' + snake_x[length - 1] + '_' + snake_y[length - 1]).css('background', color_snace);
+        $('#block_' + snake_x[length - 1] + '_' + snake_y[length - 1]).css(
+            'background',
+            color_snace
+        );
         Overstepping();
         eatApple(coordinatesApple[0], coordinatesApple[1]);
     }
@@ -107,8 +133,8 @@ $(document).ready(function () {
     function randomApple() {
         var indication = true;
         while (indication) {
-            var i = randomInteger(0, width-1),
-                j = randomInteger(0, height-1),
+            var i = randomInteger(0, width - 1),
+                j = randomInteger(0, height - 1),
                 intersection = false;
             for (var l = 0; l < length; l++) {
                 if (snake_x[l] === i && snake_y[l] === j) {
@@ -175,11 +201,17 @@ $(document).ready(function () {
         });
     }
 
-    var width = 30, height = 30, speed = 100;
+    var width = 30,
+        height = 30,
+        speed = 100;
     createField(width, height); //создаем поле 30х30
     var length = 3,
-        snake_x = [0, 1, 2],
-        snake_y = [0, 0, 0],
+        snake_x = [
+            0, 1, 2
+        ],
+        snake_y = [
+            0, 0, 0
+        ],
         lastTouch = 40,
         counter = 0,
         best_game = 0,
